c500050735ed359939353c3e076f7842ed7f06c8
Abhishek Nema <nemaabhishek3@gmail.com>
2025-06-15 17:48:56 +0530

    completed Inception of Namaste React

---
+++ 01_Inception/04_App.js
@@ -0,0 +1,65 @@
+// creating React element or Javascript object
+const h1 = React.createElement(
+  "h1",
+  { id: "h1_tag" },
+  "Hello World!, from your freindly React"
+);
+
+// creating React root
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// converting react element into h1 tag in browser
+root.render(h1);
+
+// -----------------------------------------------------
+// creating anchor tag using React
+const hyperLink = React.createElement(
+  "a",
+  { href: "https://www.google.com", target: "_blank" },
+  "Google"
+);
+
+const a_tag_container = ReactDOM.createRoot(document.getElementById("a_tag"));
+
+a_tag_container.render(hyperLink);
+
+// -----------------------------------------------------
+
+/*
+let's create this HTML structure in React...
+
+<div id="parent">
+        <div id="child1">
+            <h1>Hi, I'm H1 tag</h1>
+            <h2>Hi, I'm H2 tag</h2>
+        </div>
+        <div id="child2">
+            <h1>Hi, I'm H1 tag</h1>
+            <h2>Hi, I'm H2 tag</h2>
+        </div>
+    </div>
+*/
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "div",
+    { id: "child1" },
+    // To create siblings of children, pass them in array
+    [
+      React.createElement("h1", {}, "Hi, I'm H1 tag"),
+      React.createElement("h3", {}, "Hi, I'm H3 tag"),
+    ]
+  ),
+  React.createElement(
+    "div",
+    { id: "child2" },
+    // To create siblings of children, pass them in array
+    [
+      React.createElement("h1", {}, "Hi, I'm H1 tag"),
+      React.createElement("h3", {}, "Hi, I'm H3 tag"),
+    ]
+  ),
+]);
+
+// render() will convert React Element into HTML tag inside brwoser
+root.render(parent);
---
+++ 01_Inception/Test.js
@@ -0,0 +1,7 @@
+const h1 = React.createElement("h1", { id: "h1_tag" }, "I am h1");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(h1);
+
+const h3 = React.createElement("h3", { id: "h3_tag" }, "I am h3");
+const abhi_obj = ReactDOM.createRoot(document.getElementById("abhi"));
+abhi_obj.render(h3);
