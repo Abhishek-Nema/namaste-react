274eddd14457773b816370d35a294f98fe34eebf
Abhishek Nema <nemaabhishek3@gmail.com>
2025-06-22 17:13:26 +0530

    completed basics of chap. 3

--- 03_Laying_the_Foundation/App.js
+++ 03_Laying_the_Foundation/App.js
@@ -1,5 +1,5 @@
 // importing React for core react functionality
-import React from "react";
+import React, { Component } from "react";
 
 // importing ReactDOM for DOM functionality
 import ReactDOM from "react-dom/client";
@@ -15,6 +15,64 @@ root.render(heading_h1);
 // ----------------------------------------------------
 // creating and rendering React element using JSX
 
-const heading_h2 = <h1>This is H2 using JSX</h1>;
-
+// way1
+const heading_h2 = <h2>This is H2 using JSX</h2>;
 root.render(heading_h2);
+
+// way2
+const heading_h3 = (
+  <div>
+    <h3>This is H3 using JSX</h3>
+  </div>
+);
+root.render(heading_h3);
+// ----------------------------------------------------
+// creating Functional components
+
+// way1
+const HeadingComp1 = () => {
+  return <div>this is heading1</div>;
+};
+root.render(<HeadingComp1 />);
+
+// way2
+const HeadingComp2 = () => <div>this is heading2</div>;
+root.render(<HeadingComp2 />);
+
+// way3
+const HeadingComp3 = () => (
+  <span>
+    <div>this is heading3</div>
+  </span>
+);
+root.render(<HeadingComp3 />);
+
+// Component Composition
+const H1_component = () => {
+  return <h1>I'm H1 tag</h1>;
+};
+
+const Title = () => (
+  <div>
+    <H1_component />
+    <p>Hi, this is title</p>
+    {/* let's add element into component */}
+    {/* {heading_h3} */}
+  </div>
+);
+
+root.render(<Title />);
+
+// let's add component into jsx element
+const FooterComponent = () => {
+  return <p>I'm footer component</p>;
+};
+
+const footerElement = (
+  <footer>
+    {FooterComponent()}
+    <p>This is Footer</p>
+  </footer>
+);
+
+root.render(footerElement);
